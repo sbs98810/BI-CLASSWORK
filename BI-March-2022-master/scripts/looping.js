9973c5162ea56ae86a94305b8a5c38996d924406
sbs98810 <101046744+sbs98810@users.noreply.github.com>
2022-05-05 19:03:14 +0530

    cc

--- BI-March-2022-master/scripts/looping.js
+++ BI-March-2022-master/scripts/looping.js
@@ -38,3 +38,12 @@ do{
 }while(i<=10)*/
 
 
+const nums = [1,2,3,4,5,6,7];
+const k = 3;
+function arrayRotation(arr, numberofshifts){
+   for(let i = 0 ; i < numberofshifts;i++){
+     arr.unshift(arr.pop());
+     console.log(`step ${i +i}:`,arr);
+   }
+}
+console.log("final output:", arrayRotation(nums,k))
